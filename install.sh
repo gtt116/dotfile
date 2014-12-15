a47307cb7757da3eafb5f8a32f35cbd5a05f0cf2
gtt116 <gtt116@gmail.com>
2014-12-15 21:43:23 +0800

    using db pip mirror

--- install.sh
+++ install.sh
@@ -42,7 +42,7 @@ elif [[ $1 == 'byobu' ]]; then
     install_byobu_keybinding
 elif [[ $1 == 'all' ]]; then
     install_git
-    install_pip_mirror
+    install_pip_db_mirror
     install_byobu_keybinding
 else
     echo "Usages: $0 <git|pip|pip-db|byobu|all>"
