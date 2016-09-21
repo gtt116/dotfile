ca3f95b67f638b9acd5ac7009d6a1e0f0bee26f7
gtt116 <gtt116@gmail.com>
2016-09-21 20:39:43 +0800

    Using go1.7.1

--- install_go.sh
+++ install_go.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 set -e
-GO="go1.6.2.linux-amd64.tar.gz"
+GO="go1.7.1.linux-amd64.tar.gz"
 
 if [ -e ~/go ]; then
     echo 'golang already installed. exit'
     exit 1
 fi
 
-echo 'Downloading go1.6.2 tar..'
+echo $GO
 wget http://s.gaott.info/$GO
 
 echo 'Check sha256 of go1.6.2.tar'
-echo "e40c36ae71756198478624ed1bb4ce17597b3c19d243f3f0899bb5740d56212a $GO" |  sha256sum --check - 
+echo "43ad621c9b014cde8db17393dc108378d37bc853aa351a6c74bf6432c1bbd182 $GO" |  sha256sum --check -
 
 tar xzf $GO
 mv -v go ~/
