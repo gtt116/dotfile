d0ca3956d45e241a1d0c8bd1a51dc9b23373008d
gtt116 <gtt116@gmail.com>
2022-09-12 17:38:19 +0800

    add kube dashboard

--- k8s/master.sh
+++ k8s/master.sh
@@ -3,3 +3,10 @@ kubeadm init --pod-network-cidr=10.244.0.0/16
 
 # https://gist.github.com/rkaramandi/44c7cea91501e735ea99e356e9ae7883
 kubectl apply -f https://github.com/coreos/flannel/raw/master/Documentation/kube-flannel.yml
+
+# https://adamtheautomator.com/kubernetes-dashboard/	
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v2.6.1/aio/deploy/recommended.yaml
+
+kubectl apply admin-user.yaml
+
+kubectl -n kubernetes-dashboard create token admin-user
